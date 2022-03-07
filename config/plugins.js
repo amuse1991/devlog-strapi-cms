061c0f703a3aa742e92fc63ec5c5ddc15a09530a
amuse1991 <amuse1991@gmail.com>
2022-03-08 05:36:52 +0900

    gql, seo, commentm erc, slug 애드온 추가

---
+++ config/plugins.js
@@ -0,0 +1,46 @@
+module.exports = ({ env }) => ({
+  graphql: {
+    config: {
+      endpoint: "/graphql",
+      shadowCRUD: true,
+      playgroundAlways: false,
+      depthLimit: 7,
+      amountLimit: 100,
+      apolloServer: {
+        tracing: false,
+      },
+    },
+  },
+  slugify: {
+    enabled: true,
+    config: {
+      contentTypes: {
+        article: {
+          field: "slug",
+          references: "title",
+        },
+      },
+    },
+  },
+  seo: {
+    enabled: true,
+  },
+  comments: {
+    enabled: true,
+    config: {
+      badWords: false,
+      moderatorRoles: ["Authenticated"],
+      approvalFlow: ["api::page.page"],
+      entryLabel: {
+        "*": ["Title", "title", "Name", "name", "Subject", "subject"],
+        "api::page.page": ["MyField"],
+      },
+      reportReasons: {
+        MY_CUSTOM_REASON: "MY_CUSTOM_REASON",
+      },
+      gql: {
+        // ...
+      },
+    },
+  },
+});
